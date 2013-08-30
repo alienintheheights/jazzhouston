b2c1e61730a503fd6777c346f5484a5606e9a32a
Andrew Lienhard <alienhard@gmail.com>
2013-08-30 17:14:50 -0500

    Fix for carrierwave field name in view/controller. Required additional db:migration rake.

--- app/models/content.rb
+++ app/models/content.rb
@@ -1,14 +1,10 @@
 class Content < ActiveRecord::Base
   set_table_name "content"
   set_primary_key "content_id"
-
-  ## Thinking-Sphinx Search gem
-  #define_index do
-  #	indexes title, :sortable => true
-  #	indexes body_text
-  #	indexes display_date
-  #	indexes teaser
-  # end
+  attr_accessible :title, :content_type_id, :content_sub_type, :author, :body_text,
+				  :local_flag, :external_image_url, :photo_file_name, :author_id,
+				  :status_id, :teaser, :related_url, :html_break_flag,
+				  :display_date, :image
 
   ## CarrierWave gem
   mount_uploader :image, AvatarUploader, :mount_on => :photo_file_name
